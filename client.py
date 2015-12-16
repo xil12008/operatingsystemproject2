2d44d53a1b87ce1d5557304d48afc5ce74c5459a
Xiaoyan <luxy622@gmail.com>
2015-12-15 23:27:59 -0500

    A

--- client.py
+++ client.py
@@ -11,18 +11,11 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 s.connect((TCP_IP, TCP_PORT))
 
-buffer = "0123456789" * (100* 25820/10)
-s.send("STORE abcede.txt 2582000\n\r") 
+s.send("STORE abcede.txt 25820\n\r") 
 time.sleep(0.1)
-print "len", len(buffer)
+buffer = "-" * 25820
 s.send(buffer)
 data = s.recv(1024)
-print "recv:", data
-    
-time.sleep(10)
-
-s.send("READ abc.txt 4090 5000\n")
-data = s.recv(6000)
 print "recv:", data
 
 s.close()
